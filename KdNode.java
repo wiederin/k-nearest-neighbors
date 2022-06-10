67fe1fd5f3f129095540c956382ece9447ce399a
wiederin <wiederin@usc.edu>
2022-06-10 15:48:43 +0200

    updated node constructor - variables start with _

--- KdNode.java
+++ KdNode.java
@@ -7,10 +7,10 @@ public class KdNode {
     //public double dimensions = coords.length;
 
     // constructor
-    public KdNode(KdNode left, KdNode right, double[] coords){
-        left = left;
-        right = right;
-        coords = coords;
+    public KdNode(KdNode _left, KdNode _right, double[] _coords){
+        left = _left;
+        right = _right;
+        coords = _coords;
     }
 
     public KdNode(String coordsString) {
@@ -21,18 +21,40 @@ public class KdNode {
         }
     }
 
+    // print function
+
+    public void printNode() {
+        boolean comma = false;
+        for(double coord : coords) {
+            if(comma){System.out.print(",");}
+            else {comma = true;}
+            System.out.print(coord);
+        }
+        //System.out.println();
+    }
+
     // getters & setters
     public KdNode getLeft() {
         return left;
+    }
+
+    public void setLeft(KdNode node) {
+        left = node;
     }
 
     public KdNode getRight() {
         return right;
     }
 
+    public void setRight(KdNode node) {
+        right = node;
+    }
+
+
     public double[] getCoords() {
         return coords;
     }
+
 /*
     public void setDimensions() {
         dimensions = coords.length;
