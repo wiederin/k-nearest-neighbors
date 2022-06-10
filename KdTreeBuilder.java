b7cbf46af9d1e43721a8e8b5b8c8a046c5f41c7a
wiederin <wiederin@usc.edu>
2022-06-10 15:50:57 +0200

    init implementation to build tree & print output

--- KdTreeBuilder.java
+++ KdTreeBuilder.java
@@ -6,56 +6,56 @@ public class KdTreeBuilder {
 
         // options output
         System.out.println("---------------------------- KDTree Builder --------------------------");
-        //System.out.println("Options: ");
-        //System.out.println("0 - Build from CSV");
-        //System.out.print("Enter number of option to select: ");
+        try {
 
-        // option input
-        //int option = Integer.parseInt(System.console().readLine());
-        int option = 0;
-        if(option == 0) {
-            try {
-
-                System.out.println("Options: ");
-                // find csv files in folder
-                File csvFolder = new File("csv");
-                String[] files = csvFolder.list();
-                // display options
-                int i = 0;
-                for (String file : files) {
-                    System.out.println(i + " - " + file);
-                    i++;
-                }
-                // select
-                System.out.print("Enter the number of option to select: ");
-
-                // option input
-                int fileIndex = Integer.parseInt(System.console().readLine());
-                String filename = files[fileIndex];
-                // create scanner
-                Scanner scan = new Scanner(new File("csv/"+filename));
-                // set new line as delimiter
-                scan.useDelimiter("\n");
-                // parse file
-                while (scan.hasNext()) {
-                    String coordsString = scan.next();
-                    System.out.println(coordsString);
-                    KdNode node = new KdNode(coordsString);
-                    //System.out.println(node.getCoords());
-                    double[] coords = node.getCoords();
-                    for (double coord : coords) {
-                        System.out.println(coord);
-                    }
-                    
-                }
-                // close scan
-                scan.close();
-
-            } catch (FileNotFoundException ex) {
-                // message in case file is not found
-                System.out.println("CSV file not found");
+            System.out.println("Options: ");
+            // find csv files in folder
+            File csvFolder = new File("csv");
+            String[] files = csvFolder.list();
+            // display options
+            int i = 0;
+            for (String file : files) {
+                System.out.println(i + " - " + file);
+                i++;
             }
+            // select
+            //System.out.print("Enter the number of option to select: ");
 
+            // option input
+            //int fileIndex = Integer.parseInt(System.console().readLine());
+            int fileIndex = 0;
+            String filename = files[fileIndex];
+            // create scanner
+            Scanner scan = new Scanner(new File("csv/"+filename));
+            // set new line as delimiter
+            scan.useDelimiter("\n");
+            // parse file and build kdtree
+            KdTree tree = new KdTree();
+            int count = 0;
+            while (scan.hasNext()) {
+                // each line is coordinate string e.g. 0,0
+                String coordsString = scan.next();
+                // create node from coordinate
+                KdNode node = new KdNode(coordsString);
+                System.out.println("------------------- insert node -------------------");
+
+                // build tree
+                tree.insertNode(node, 0);
+                
+                System.out.println("-------------------- after add --------------------");
+                System.out.println("tree after adding node " + count);
+                System.out.println("tree size " + tree.getSize());
+                tree.printKdTree();
+                count++;
+                System.out.println("---------------------------------------------------");
+                    
+            }
+            // close scan
+            scan.close();
+
+        } catch (FileNotFoundException ex) {
+            // message in case file is not found
+            System.out.println("CSV file not found");
         }
     }
 }
