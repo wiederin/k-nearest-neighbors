04bf2685a88b499c40121b5ba984cbcabe088dda
wiederin <wiederin@usc.edu>
2022-06-10 23:11:18 +0200

    working implem. of KdTree

--- KdTree.java
+++ KdTree.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class KdTree {
     
     // member variables
-    public KdNode root;
+    public Node root = null;
     public int size;
     public int dimensions;
+    public Node nearest = null;
+    public double nearestDistance = 0;
+    public int visited = 0; 
 
     // constructor
-    public KdTree(KdNode _root, int _size) {
+    /*old
+    public KdTree(Node _root, int _size) {
         root = _root;
         size = _size;
         dimensions = _root.getCoords().length;
@@ -14,21 +22,112 @@ public class KdTree {
 
     public KdTree() {
         
+    }*/
+
+    public KdTree(List<Node> nodes) {
+        dimensions = nodes.get(0).getCoords().length;
+        root = makeTree(nodes, 0, nodes.size(), 0);
     }
 
+    public Node makeTree(List<Node> nodes, int begin, int end, int index) {
+        if (end <= begin) {
+            return null;
+        }
+        int n = begin + (end - begin) / 2;
+        Node node = QuickSelect.select(nodes, begin, end - 1, n, new NodeComparator(index));
+        index = (index + 1) % dimensions;
+        node.left = makeTree(nodes, begin, n, index);
+        node.right = makeTree(nodes, n + 1, end, index);
+        //printKdTree();
+        return node;
+    }
+
+    private class NodeComparator implements Comparator<Node> {
+        private int index;
+
+        private NodeComparator(int index_) {
+            index = index_;
+        }
+        public int compare(Node n1, Node n2) {
+            return Double.compare(n1.getCoord(index), n2.getCoord(index));
+        }
+
+    }
+
+    public double distance() {
+        return Math.sqrt(nearestDistance);
+    }
+
+
+    public Node findNearest(Node target) {
+        if (root == null ) {
+            throw new IllegalStateException("tree is empty!");
+        }
+        nearest = null;
+        nearestDistance = 0;
+        visited = 0;
+        findNearestRec(root, target, 0);
+        return nearest;
+    }
+
+    private void findNearestRec(Node root, Node target, int index) {
+        if (root == null) {
+            return;
+        }
+        ++visited;
+        double dist = root.distance(target);
+        if (nearest == null || dist < nearestDistance) {
+            nearestDistance = dist;
+            nearest = root;
+        }
+        if (nearestDistance == 0) {
+            return;
+        }
+        double depth = root.getCoord(index) - target.getCoord(index);
+        index = (index + 1) % dimensions;
+        findNearestRec(depth > 0 ? root.left : root.right, target, index);
+        if (depth * depth >= nearestDistance) {
+            return;
+        }
+        findNearestRec(depth > 0 ? root.right : root.left, target, index);
+    }
+    
     // print function
     public void printKdTree() {
-        System.out.println("-------------------- print tree --------------------");
-        KdNode[] nodes = new KdNode[2];
-        System.out.print("1      ");
+        System.out.println("----------------------------- print tree -----------------------------");
+        List<Node> nodes = new ArrayList();
+        System.out.print("0      ");
         root.printNode();
         System.out.println();
-        if(size == 0) {return;}
-        nodes[0] = root.getLeft();
-        nodes[1] = root.getRight();
+        if(root.getLeft() == null && root.getRight() == null) {return;}
+        nodes.add(root.getLeft());
+        nodes.add(root.getRight());
+        for(int i = 0; i < root.getHeight(); i++) {
+            System.out.print(i+1 + " ");
+            List<Node> nodesNext = new ArrayList();
+            int index = 0;
+            for(Node node: nodes) {
+                if(index != 0) {
+                    System.out.print(" ");
+                }
+                ++index;
+                if(node == null) {
+                    System.out.print(" - ");
+                    nodesNext.add(null);
+                    nodesNext.add(null);
+                } else {
+                    node.printNode();
+                    nodesNext.add(node.left);
+                    nodesNext.add(node.right);
+                }
+            }
+            System.out.println();
+            nodes = nodesNext;
+        }
+        /*
         for (int i = 1; i < size + 1; i++) {
             int index = 0;
-            KdNode[] nodesTemp = new KdNode[(i+1)*2];
+            Node[] nodesTemp = new Node[(i+1)*2];
             System.out.print((i)*2 + " ");
             for (int j = 0; j < nodes.length; j++) {
 
@@ -51,7 +150,7 @@ public class KdTree {
             }
             System.out.println();
             System.out.print("nodes ");
-            for(KdNode node: nodesTemp) {
+            for(Node node: nodesTemp) {
                 if(node != null){
                     node.printNode();
                 } else {
@@ -60,13 +159,11 @@ public class KdTree {
             }
             System.out.println();
             nodes = nodesTemp.clone();
-            
-
-        }
+        }*/
     }
-
+    /*
     // insert function
-    public void insertNode(KdNode node, int depth) {
+    public void insertNode(Node node, int depth) {
         // compare with root
         //KdNode rootTemp = root;
         if(root == null) {
@@ -112,7 +209,7 @@ public class KdTree {
                 if(root.getRight().getLeft() != null) {
                     root.getRight().getLeft().printNode();
                     printKdTree();
-                }*/
+                }*/ /*
 
                 //subTree.getRoot().getLeft().printNode();
                 //depth += 2;
@@ -120,10 +217,10 @@ public class KdTree {
 
             }
         }
-    }
+    } */
 
     // getters & setters
-    public KdNode getRoot() {
+    public Node getRoot() {
         return root;
     }
 
