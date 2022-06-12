8c7fac24b270dfe5d6b9c7cc0976c37e49729434
wiederin <wiederin@usc.edu>
2022-06-12 13:56:26 +0200

    worked on tree print function

--- KdTree.java
+++ KdTree.java
@@ -13,17 +13,6 @@ public class KdTree {
     public int visited = 0; 
 
     // constructor
-    /*old
-    public KdTree(Node _root, int _size) {
-        root = _root;
-        size = _size;
-        dimensions = _root.getCoords().length;
-    }
-
-    public KdTree() {
-        
-    }*/
-
     public KdTree(List<Node> nodes) {
         dimensions = nodes.get(0).getCoords().length;
         root = makeTree(nodes, 0, nodes.size(), 0);
@@ -66,11 +55,11 @@ public class KdTree {
         nearest = null;
         nearestDistance = 0;
         visited = 0;
-        findNearestRec(root, target, 0);
+        findNearest(root, target, 0);
         return nearest;
     }
 
-    private void findNearestRec(Node root, Node target, int index) {
+    private void findNearest(Node root, Node target, int index) {
         if (root == null) {
             return;
         }
@@ -85,25 +74,35 @@ public class KdTree {
         }
         double depth = root.getCoord(index) - target.getCoord(index);
         index = (index + 1) % dimensions;
-        findNearestRec(depth > 0 ? root.left : root.right, target, index);
+        findNearest(depth > 0 ? root.left : root.right, target, index);
         if (depth * depth >= nearestDistance) {
             return;
         }
-        findNearestRec(depth > 0 ? root.right : root.left, target, index);
+        findNearest(depth > 0 ? root.right : root.left, target, index);
     }
     
     // print function
     public void printKdTree() {
         System.out.println("----------------------------- print tree -----------------------------");
         List<Node> nodes = new ArrayList();
-        System.out.print("0      ");
+        System.out.print("0 ");
+        for(int k = 0; k < root.getHeight(); k++) {
+            System.out.print("      ");
+        }
         root.printNode();
         System.out.println();
         if(root.getLeft() == null && root.getRight() == null) {return;}
         nodes.add(root.getLeft());
         nodes.add(root.getRight());
         for(int i = 0; i < root.getHeight(); i++) {
-            System.out.print(i+1 + " ");
+            for(int l = 0; l < 20/(i+1); l++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j <= i; j++) {
+                System.out.print(" /   \\   ");
+            }
+            System.out.println();
+            System.out.print(i+1 + "   ");
             List<Node> nodesNext = new ArrayList();
             int index = 0;
             for(Node node: nodes) {
@@ -124,100 +123,9 @@ public class KdTree {
             System.out.println();
             nodes = nodesNext;
         }
-        /*
-        for (int i = 1; i < size + 1; i++) {
-            int index = 0;
-            Node[] nodesTemp = new Node[(i+1)*2];
-            System.out.print((i)*2 + " ");
-            for (int j = 0; j < nodes.length; j++) {
-
-                //System.out.print(nodes.length);
-                if(nodes[j] != null){
-                    nodes[j].printNode();
-                    System.out.println();
-                    System.out.print("adding :");
-                    if(nodes[j].getLeft()!=null){nodes[j].getLeft().printNode();}
-                    if(nodes[j].getRight()!=null){nodes[j].getRight().printNode();}
-                    nodesTemp[index] = nodes[j].getLeft();
-                    if(nodesTemp[index]!=null){nodesTemp[index].printNode();}
-                    nodesTemp[index++] = nodes[j].getRight();
-                    if(nodesTemp[index+1]!=null){nodesTemp[index].printNode();}
-                } else {
-                    System.out.print(" - ");
-                    nodesTemp[index] = null;
-                    nodesTemp[index++] = null;
-                }
-            }
-            System.out.println();
-            System.out.print("nodes ");
-            for(Node node: nodesTemp) {
-                if(node != null){
-                    node.printNode();
-                } else {
-                    System.out.print("-");
-                }
-            }
-            System.out.println();
-            nodes = nodesTemp.clone();
-        }*/
+        System.out.println("----------------------------------------------------------------------");
+    
     }
-    /*
-    // insert function
-    public void insertNode(Node node, int depth) {
-        // compare with root
-        //KdNode rootTemp = root;
-        if(root == null) {
-            root = node;
-            size = 0;
-            dimensions = root.getCoords().length;
-            return;
-        }
-        System.out.println("depth " + depth + " comparing " + root.getCoords()[depth%dimensions] + " to " + node.getCoords()[depth%dimensions]);
-        if(root.getCoords()[depth%dimensions] > node.getCoords()[depth%dimensions]){
-            // go left or set left to node if left is null
-            if(root.getLeft()==null) {
-                System.out.println("Set left of root");
-                root.setLeft(node);
-                size += 1;
-                //printKdTree();
-                //root.getLeft().printNode();
-            } else {
-                System.out.println("Go Left");
-                KdTree subTree = new KdTree(root.getLeft(), size-1);
-                
-                subTree.insertNode(node, depth++);
-                root.setLeft(subTree.getRoot());
-            }
-        } else {
-            // go right or set right to node if right is null
-            if(root.getRight()==null) {
-                System.out.println("Set right of root");
-                root.setRight(node);
-                size += 1;
-            } else {
-                System.out.println("Go Right");
-                KdTree subTree = new KdTree(root.getRight(), size-1);
-                System.out.println("comparing " + root.getRight().getCoords()[depth+1%dimensions] + " to " + node.getCoords()[depth+1%dimensions]);
-                System.out.print("insert ");
-                node.printNode();
-                subTree.insertNode(node, ++depth);
-                //subTree.printKdTree();
-                root.setRight(subTree.getRoot());
-                size += depth;
-                //printKdTree();
-                /*root.getRight().printNode();
-                if(root.getRight().getLeft() != null) {
-                    root.getRight().getLeft().printNode();
-                    printKdTree();
-                }*/ /*
-
-                //subTree.getRoot().getLeft().printNode();
-                //depth += 2;
-                //root.setRight(subTree.getRoot());
-
-            }
-        }
-    } */
 
     // getters & setters
     public Node getRoot() {
