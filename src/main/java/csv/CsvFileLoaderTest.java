1dd3db747f834aa4cf2fe0e524b736378cece59b
wiederin <wiederin@usc.edu>
2022-08-05 23:04:53 +0200

    CsvFileLoader and tests impl

--- src/main/java/csv/CsvFileLoaderTest.java
+++
@@ -1,9 +0,0 @@
-package src.main.java.csv;
-import org.junit.Test;
-
-public class CsvFileLoaderTest {
-    @Test
-    public void csvFileLoaderTest() {
-        
-    }
-}
---
+++ src/main/java/kdtree/csv/CsvFileLoader.java
@@ -0,0 +1,41 @@
+package src.main.java.kdtree.csv;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import src.main.java.kdtree.tree.node.Node;
+
+public class CsvFileLoader{
+    // member variables
+    public List<Node> nodes = new ArrayList<Node>();
+
+    // method to load csv data 
+    public boolean load(String filename) {
+            try {
+                // create scanner
+                Scanner scan = new Scanner(new File(filename));
+                // set new line as delimiter
+                scan.useDelimiter("\n");
+                // parse file
+                // int count = 0;
+                while (scan.hasNext()) {
+                    // each line is coordinate string e.g. 0,0
+                    String coordsString = scan.next();
+                    // create node from coordinate
+                    Node node = new Node(coordsString);
+                    nodes.add(node);
+                        
+                }
+                // close scan
+                scan.close();
+                return true;
+    
+            } catch (FileNotFoundException ex) {
+                // message in case file is not found
+                System.out.println("CSV file not found.");
+                return false;
+            }
+    }
+}
